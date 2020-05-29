491643bfb139a4bca1e48a9f3ac0b5f8448f18de
comeschffr <come.schffr@gmail.com>
2020-05-29 22:13:37 +0200

    Backend challenge

---
+++ backend.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/qaanswer'
+
+db = SQLAlchemy(app)
+
+@app.route('/set')
+def set():
+	for somekey, somevalue in request.args.items():
+		thing = Something(key=somekey, value=somevalue)
+		db.session.add(thing)
+	db.session.commit()
+	return f'SET: {somekey}={somevalue}'
+
+
+@app.route('/get')
+def get():
+	somekey = request.args.get('key')
+	somevalue = Something.query.filter_by(key=somekey).first().value
+	return f'GET: {somekey}={somevalue}'
+
+
+class Something(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key = db.Column(db.String(200), unique=True, nullable=False)
+	value = db.Column(db.String(200), unique=True, nullable=False)
+
+
+if __name__ == '__main__':
+	app.run(debug=True, port=4000)
